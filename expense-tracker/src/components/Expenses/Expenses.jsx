19774cdc7a93e0a52c37331ec727c752cad809cb
Patrick Hettich <patrick_hettich@hotmail.com>
2022-03-07 21:01:09 +0100

    finished two way binding

--- expense-tracker/src/components/Expenses/Expenses.jsx
+++ expense-tracker/src/components/Expenses/Expenses.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filterDate, setFilterDate] = useState('2022');
+
+    const onUpdateFilterDate = (date) => {
+        setFilterDate(date);
+    }
+
     return (
-        <Card className='expenses'>
-            {
-                props.expenses.map(expense => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))
-            }
-        </Card>
+        <div>
+            <ExpensesFilter onUpdateFilterDate={onUpdateFilterDate} currentSelected={filterDate} />
+            <Card className='expenses'>
+                {
+                    props.expenses.map(expense => (
+                        <ExpenseItem
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />
+                    ))
+                }
+            </Card>
+        </div>
     );
 }
 
