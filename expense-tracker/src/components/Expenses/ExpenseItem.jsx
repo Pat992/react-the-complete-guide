f79f70dea681880850e8168f3494c1b3ce8e9dca
Patrick Hettich <patrick_hettich@hotmail.com>
2022-03-07 21:03:37 +0100

    reset item into stless-component

--- expense-tracker/src/components/Expenses/ExpenseItem.jsx
+++ expense-tracker/src/components/Expenses/ExpenseItem.jsx
@@ -4,19 +4,13 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-
-    const onClickHandler = () => {
-        setTitle('Updated');
-    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div>
-                <h2 className='expense-item__description'>{title}</h2>
+                <h2 className='expense-item__description'>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount} CHF</div>
             </div>
-            <button onClick={onClickHandler}>Change Title</button>
         </Card>
     );
 }
