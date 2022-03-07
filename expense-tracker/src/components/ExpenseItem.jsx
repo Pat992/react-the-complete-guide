421a0a739961073a88fc2f73a95d7028b68dddf8
Patrick Hettich <patrick_hettich@hotmail.com>
2022-03-07 18:50:36 +0100

    added props to expense item

--- expense-tracker/src/components/ExpenseItem.jsx
+++ expense-tracker/src/components/ExpenseItem.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+    const expenseDate = new Date(2022, 2, 7);
+    const expenseTitle = 'Rent';
+    const amount = 1950.00;
+
     return (
-        <section className='expense-item'>
-            <div>Date</div>
+        <div className='expense-item'>
+            <div>{props.date.toDateString()}</div>
             <div>
-                <h2 className='expense-item__description'>Title</h2>
-                <div className='expense-item__price'>Amount</div>
+                <h2 className='expense-item__description'>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount.toFixed(2)} CHF</div>
             </div>
-        </section>
+        </div>
     );
 }
 
