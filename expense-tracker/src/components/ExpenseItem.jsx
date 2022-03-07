f873e0c632320c44c483d08f5c99f6229c422f59
Patrick Hettich <patrick_hettich@hotmail.com>
2022-03-07 19:03:11 +0100

    split expense item into date

--- expense-tracker/src/components/ExpenseItem.jsx
+++ expense-tracker/src/components/ExpenseItem.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    const expenseDate = new Date(2022, 2, 7);
-    const expenseTitle = 'Rent';
-    const amount = 1950.00;
-
     return (
         <div className='expense-item'>
-            <div>{props.date.toDateString()}</div>
+            <ExpenseDate date={props.date}></ExpenseDate>
             <div>
                 <h2 className='expense-item__description'>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount.toFixed(2)} CHF</div>
