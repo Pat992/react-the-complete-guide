f9262c4f419390fcdcdc6476148f19a51630cb36
Patrick Hettich <patrick_hettich@hotmail.com>
2022-04-30 20:00:36 +0200

    added error value if empty text

--- course-goals/src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ course-goals/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -5,21 +5,31 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.length > 0) {
+      setIsValid(prevSnapshot => true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(prevSnapshot => false);
+      return;
+    }
+    setIsValid(prevSnapshot => true);
+    setEnteredValue(prevSnapshot => '');
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input style={{ borderColor: !isValid ? 'red' : '#ccc', background: !isValid ? 'salmon' : 'transparent' }} type="text" onChange={goalInputChangeHandler} value={enteredValue} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
