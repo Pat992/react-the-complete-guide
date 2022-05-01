37586469f95bbaebabe60f63128152ab2243cf11
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-01 11:23:48 +0200

    added users-list

--- users-app/src/App.js
+++ users-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 const App = () => {
   return (
     <div>
       <AddUser />
+      <UsersList users={[{ name: 'GAGAMAN', age: 115 }]} />
     </div>
   );
 }
