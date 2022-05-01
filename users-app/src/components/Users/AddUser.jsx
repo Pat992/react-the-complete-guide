bca9bfaf73cf462ad1b2713b11490e266ae1e599
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-01 10:53:26 +0200

    added card component, with styles

--- users-app/src/components/Users/AddUser.jsx
+++ users-app/src/components/Users/AddUser.jsx
@@ -1,5 +1,8 @@
 // @ts-check
 import { useState } from 'react';
+import Card from '../UI/Card';
+
+import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
@@ -7,13 +10,15 @@ const AddUser = (props) => {
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input type="text" name='username' id='username' />
-            <label htmlFor="age">Age</label>
-            <input type="number" id='age' name='age' />
-            <button type='submit'>Add user</button>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="text" name='username' id='username' />
+                <label htmlFor="age">Age</label>
+                <input type="number" id='age' name='age' />
+                <button type='submit'>Add user</button>
+            </form>
+        </Card>
     );
 };
 export default AddUser;
