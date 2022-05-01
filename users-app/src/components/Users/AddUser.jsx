158a2a54593ddcb09d0327e0badab3b6ce55ba1d
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-01 12:56:04 +0200

    added ref in user-form

--- users-app/src/components/Users/AddUser.jsx
+++ users-app/src/components/Users/AddUser.jsx
@@ -1,5 +1,5 @@
 // @ts-check
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
@@ -7,32 +7,50 @@ import ErrorModal from '../UI/ErrorModal';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
-    const [enteredName, setEnteredName] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    // ref for input -> currentProb = current value
+    // ref is used mostly if the value is readonly
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
+    // const [enteredName, setEnteredName] = useState('');
+    // const [enteredAge, setEnteredAge] = useState('');
     const [errorState, setErrorState] = useState({ hasError: false, errorTitle: '', errorText: '' });
 
     const addUserHandler = (event) => {
         event.preventDefault();
 
-        if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
+        // You could do everything with ref and therefore remove the useState for both inputs
+        // @ts-ignore
+        const enteredUName = nameInputRef.current.value;
+        // @ts-ignore
+        const enteredUAge = ageInputRef.current.value;
+
+        if (enteredUName.trim().length === 0 || enteredUAge.trim().length === 0) {
             setError('Invalid input', 'Please enter a name and age');
             return;
         }
 
-        if (+enteredAge < 1) {
+        if (+enteredUAge < 1) {
             setError('Invalid input', 'Age must be bigger then 0');
             return;
         }
         setError('', '');
 
-        props.addUserHandler(enteredName, enteredAge);
+        props.addUserHandler(enteredUName, enteredUAge);
 
-        setEnteredName(prevSnapshot => '');
-        setEnteredAge(prevSnapshot => '');
+        // should not manipulate DOM like this, but in this case it is okeish...
+        // @ts-ignore
+        nameInputRef.current.value = '';
+        // @ts-ignore
+        ageInputRef.current.value = '';
+
+        // No need to useState -> useRef is working
+        // setEnteredName(_ => '');
+        // setEnteredAge(_ => '');
     }
 
     const setError = (title, msg) => {
-        setErrorState(prevSnapshot => {
+        setErrorState(_ => {
             return {
                 hasError: title.trim().length === 0 ? false : true,
                 errorTitle: title,
@@ -41,13 +59,14 @@ const AddUser = (props) => {
         });
     }
 
-    const inputNameHandler = (event) => {
-        setEnteredName(prevSnapshot => event.target.value);
-    }
+    // Once again, not necessary if useRef() is being used
+    // const inputNameHandler = (event) => {
+    //     setEnteredName(_ => event.target.value);
+    // }
 
-    const inputAgeHandler = (event) => {
-        setEnteredAge(prevSnapshot => event.target.value);
-    }
+    // const inputAgeHandler = (event) => {
+    //     setEnteredAge(_ => event.target.value);
+    // }
 
     return (
         <React.Fragment>
@@ -55,9 +74,25 @@ const AddUser = (props) => {
             <Card className={styles.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
-                    <input type="text" name='username' id='username' value={enteredName} onChange={inputNameHandler} />
+                    <input
+                        type="text"
+                        name='username'
+                        id='username'
+                        // Not necessary if ref is set
+                        // value={enteredName}
+                        // onChange={inputNameHandler}
+                        ref={nameInputRef}
+                    />
                     <label htmlFor="age">Age</label>
-                    <input type="number" id='age' name='age' value={enteredAge} onChange={inputAgeHandler} />
+                    <input
+                        type="number"
+                        id='age'
+                        name='age'
+                        // Not necessary if ref is set
+                        // value={enteredAge}
+                        // onChange={inputAgeHandler}
+                        ref={ageInputRef}
+                    />
                     <Button type='submit'>Add user</Button>
                 </form>
             </Card>
