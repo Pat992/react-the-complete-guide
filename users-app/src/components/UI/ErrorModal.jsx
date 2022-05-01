282ec15ecb8f9e24c422d041b677a01287f76289
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-01 12:40:46 +0200

    added Portal for Modal

--- users-app/src/components/UI/ErrorModal.jsx
+++ users-app/src/components/UI/ErrorModal.jsx
@@ -3,24 +3,33 @@
 import styles from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
-import React from 'react';
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+const Backdrop = (props) => <div className={styles.backdrop} onClick={props.closeModal} />;
+
+const ModalOverlay = (props) => (
+    <Card className={styles.modal}>
+        <header className={styles.header}>
+            <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+            <p>{props.msg}</p>
+        </div>
+        <footer className={styles.footer}>
+            <Button onClick={props.closeModal}>Close</Button>
+        </footer>
+    </Card>
+);
 
 const ErrorModal = (props) => {
+
     return (
-        <React.Fragment>
-            <div className={styles.backdrop} onClick={props.closeModal}></div>
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.msg}</p>
-                </div>
-                <footer className={styles.footer}>
-                    <Button onClick={props.closeModal}>Close</Button>
-                </footer>
-            </Card>
-        </React.Fragment>
+        <Fragment>
+            {/* Use Portal to set the Modal in the root */}
+            {ReactDOM.createPortal(<Backdrop closeModal={props.closeModal} />, document.querySelector('#backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay closeModal={props.closeModal} title={props.title} msg={props.msg} />, document.querySelector('#overlay-root'))}
+        </Fragment>
     );
 };
 
