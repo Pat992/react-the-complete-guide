bad0d94c2a1d06948c6bbb2ec54497282c8c5c7d
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-01 11:07:00 +0200

    added form-handler

--- users-app/src/components/Users/AddUser.jsx
+++ users-app/src/components/Users/AddUser.jsx
@@ -6,17 +6,28 @@ import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+    }
+
+    const inputNameHandler = (event) => {
+        setEnteredName(prevSnapshot => event.target.value);
+    }
+
+    const inputAgeHandler = (event) => {
+        setEnteredAge(prevSnapshot => event.target.value);
     }
 
     return (
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input type="text" name='username' id='username' />
+                <input type="text" name='username' id='username' value={enteredName} onChange={inputNameHandler} />
                 <label htmlFor="age">Age</label>
-                <input type="number" id='age' name='age' />
+                <input type="number" id='age' name='age' value={enteredAge} onChange={inputAgeHandler} />
                 <Button type='submit'>Add user</Button>
             </form>
         </Card>
