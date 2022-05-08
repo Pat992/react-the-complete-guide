108d869e5418160a487eed81df25f15413b5c1e5
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-08 10:19:51 +0200

    login saved on local storage

--- 10-login-app/src/App.js
+++ 10-login-app/src/App.js
@@ -1,4 +1,4 @@
-import { useState, Fragment } from 'react';
+import { useState, Fragment, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,24 @@ import MainHeader from './components/MainHeader/MainHeader';
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    // Do not run in the component, else you will have a nice endless-loop
+    const storedLoggedIn = localStorage.getItem('isLoggedIn');
+    if (storedLoggedIn === '1') {
+      setIsLoggedIn(snapshot => true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    // Save to local storage
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
