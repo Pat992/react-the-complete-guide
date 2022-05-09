f2b483defe8925ea3dc080cc329512e81313c834
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-09 18:52:39 +0200

    init context api

--- 10-login-app/src/components/MainHeader/Navigation.jsx
+++ 10-login-app/src/components/MainHeader/Navigation.jsx
@@ -1,27 +1,61 @@
+import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(context) => {
+        return <nav className={classes.nav}>
+          <ul>
+            {context.isLoggedIn && (
+              <li>
+                <a href="/">Users</a>
+              </li>
+            )}
+            {context.isLoggedIn && (
+              <li>
+                <a href="/">Admin</a>
+              </li>
+            )}
+            {context.isLoggedIn && (
+              <li>
+                <button onClick={props.onLogout}>Logout</button>
+              </li>
+            )}
+          </ul>
+        </nav>
+      }}
+    </AuthContext.Consumer>
   );
 };
 
 export default Navigation;
+
+// Context API -> CONSUMER
+// const Navigation = (props) => {
+//   return (
+//     <AuthContext.Consumer>
+//       {(context) => {
+//         return <nav className={classes.nav}>
+//           <ul>
+//             {context.isLoggedIn && (
+//               <li>
+//                 <a href="/">Users</a>
+//               </li>
+//             )}
+//             {context.isLoggedIn && (
+//               <li>
+//                 <a href="/">Admin</a>
+//               </li>
+//             )}
+//             {context.isLoggedIn && (
+//               <li>
+//                 <button onClick={props.onLogout}>Logout</button>
+//               </li>
+//             )}
+//           </ul>
+//         </nav>
+//       }}
+//     </AuthContext.Consumer>
+//   );
+// };
---
+++ 10-login-app/src/store/auth-context.js
@@ -0,0 +1,8 @@
+//@ts-check
+import React from "react";
+
+const AuthContext = React.createContext({
+    isLoggedIn: false
+});
+
+export default AuthContext;
