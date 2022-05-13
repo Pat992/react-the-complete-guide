4b18270ac75e794d6ba680b034928c7cae86401a
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-13 18:31:45 +0200

    added cart

--- 11-food-order-app/src/components/Cart/Cart.jsx
+++ 11-food-order-app/src/components/Cart/Cart.jsx
@@ -4,8 +4,28 @@
 import styles from './Cart.module.css';
 
 const Cart = (props) => {
+    const CART_ITEMS = <ul className={styles['cart-items']}>{
+        [
+            {
+                id: 'c1',
+                name: 'Sushi',
+                amount: 2,
+                price: 12.99
+            }
+        ].map(item => <li>{item.name}</li>)
+    }</ul>;
+
     return (
         <div>
+            {CART_ITEMS}
+            <div className={styles.total}>
+                <span>Total amount</span>
+                <span>35.69</span>
+            </div>
+            <div className={styles.actions}>
+                <button className={styles['button--alt']}>Close</button>
+                <button className={styles.button}>Order</button>
+            </div>
         </div>
     );
 };
