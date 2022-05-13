59c0d64cbf5d0a843c8996e36e3efc730d16f006
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-13 19:25:55 +0200

    using context and added reducer

--- 11-food-order-app/src/components/UI/CartButton.jsx
+++ 11-food-order-app/src/components/UI/CartButton.jsx
@@ -3,13 +3,21 @@
 import CartIcon from './CartIcon';
 // @ts-ignore
 import styles from './CartButton.module.css';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 const CartButton = (props) => {
+    const cartContext = useContext(CartContext);
+
+    const numOfCartItems = cartContext.items.reduce((currentNum, item) => {
+        return currentNum + item.amount;
+    }, 0);
+
     return (
         <button onClick={props.onShowCart} className={styles.button}>
             <span className={styles.icon}><CartIcon /></span>
             <span>Your Cart</span>
-            <span className={styles.badge}>2</span>
+            <span className={styles.badge}>{numOfCartItems}</span>
         </button>
     );
 };
--- 11-food-order-app/src/store/CartProvider.js
+++ 11-food-order-app/src/store/CartProvider.js
@@ -1,12 +1,41 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+// Outside of component to not recreate functions
+const defaultCartState = {
+    items: [],
+    amount: 0
+}
+
+// Never change old snapshot, only create new states
+const cartReducer = (stateSnapshot, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const updatedItems = stateSnapshot.items.concat(action.item);
+        const updatedTotal = stateSnapshot.amount + action.item.price * action.item.amount;
+
+        return {
+            items: updatedItems,
+            amount: updatedTotal
+        };
+    } else if (action.type === 'REMOVE_ITEM') {
+
+    }
+    return defaultCartState;
+};
+
 const CartProvider = (props) => {
-    const addItemHandler = (item) => { }
-    const removeItemHandler = (item) => { }
+    const [cartState, setCartState] = useReducer(cartReducer, defaultCartState);
+
+    const addItemHandler = (item) => {
+        setCartState({ type: 'ADD_ITEM', item: item });
+    };
+    const removeItemHandler = (id) => {
+        setCartState({ type: 'REMOVE_ITEM', id: id });
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.amount,
         addItem: addItemHandler,
         removeItem: removeItemHandler
     }
