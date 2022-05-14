dd0f1060702282fb064fb2cf4ba74f6de06516df
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-14 16:11:49 +0200

    added useEffect for animation

--- 11-food-order-app/src/components/UI/CartButton.jsx
+++ 11-food-order-app/src/components/UI/CartButton.jsx
@@ -1,20 +1,41 @@
 // @ts-check
-
+import { useContext, useEffect, useState } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from './CartIcon';
+
 // @ts-ignore
 import styles from './CartButton.module.css';
-import { useContext } from 'react';
-import CartContext from '../../store/cart-context';
 
 const CartButton = (props) => {
+    const [animate, setAnimate] = useState(false);
     const cartContext = useContext(CartContext);
+
+    const { items } = cartContext;
+
+    const buttonClasses = `${styles.button} ${animate ? styles.bump : ''}`;
+
+    useEffect(() => {
+        // add and remove bump class
+        if (cartContext.items.length <= 0) {
+            return;
+        }
+        setAnimate(_ => true);
+        const timer = setTimeout(() => {
+            setAnimate(_ => false);
+        }, 100);
+
+        // cleanup
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
 
     const numOfCartItems = cartContext.items.reduce((currentNum, item) => {
         return currentNum + item.amount;
     }, 0);
 
     return (
-        <button onClick={props.onShowCart} className={styles.button}>
+        <button onClick={props.onShowCart} className={buttonClasses}>
             <span className={styles.icon}><CartIcon /></span>
             <span>Your Cart</span>
             <span className={styles.badge}>{numOfCartItems}</span>
