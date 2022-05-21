ca40846e5d9c5bf5c44fd3319b3aa95ed9fe99f4
Patrick Hettich <patrick_hettich@hotmail.com>
2022-05-21 09:26:52 +0200

    init section 13

---
+++ 13-class-based/src/components/User.js
@@ -0,0 +1,11 @@
+import classes from './User.module.css';
+
+class User {
+  render() { }
+}
+
+const User = (props) => {
+  return <li className={classes.user}>{props.name}</li>;
+};
+
+export default User;
